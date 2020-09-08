1cce11fe2bd3c72418cb12b509767d9a4d711237
Rem Kim <kim.rmi.93@gmail.com>
2020-09-08 14:04:26 -0400

    solution
    Signed-off-by: Rem Kim <kim.rmi.93@gmail.com>

--- src/PokemonGenerator.js
+++ src/PokemonGenerator.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import {
   Query, // component
   useQuery // hook
@@ -7,12 +7,33 @@ import getPokemons from './api/getPokemons';
 
 const PokemonGenerator = () =>
 {
+  const [pokemons, setState] = useState([])
+  const [randomPokemonIdx, setPokemon] = useState(0)
+  const [res] = useQuery({
+    query: getPokemons
+  })
+  useEffect(() => {
+    if(typeof res.data !== 'undefined' && !res.fetching && !res.error){
+      console.log("setting pokemons", res.data.pokemons)
+      setState(res.data.pokemons);
+    }
+  }, [res.data, res.error, res.fetching])
+
+  if(res.error) return "Oops failed to load Pokemons"
+  if(res.fetching || !pokemons.length) return "Loading"
+
+  const randomize = () => {
+    setPokemon(Math.floor(Math.random() * 150))
+  }
+
   return (
   <Fragment>
-    <h1>🤔</h1>
+    <h1>{pokemons[randomPokemonIdx].name}</h1>
+    <h2>{pokemons[randomPokemonIdx].classification}</h2>
     <p>
-      Hmm... Needs more Pokemon
+      <img src={pokemons[randomPokemonIdx].image} alt={pokemons[randomPokemonIdx].name} />
     </p>
+    <button onClick={randomize}>Get Random Pokemon!</button>
   </Fragment>
 )}
 
--- src/api/getPokemons.js
+++ src/api/getPokemons.js
@@ -1,6 +1,10 @@
 const getPokemons = `
   query GetPokemons {
-    pokemons{
+    pokemons(first: 150){
+      id
+      name
+      classification
+      image
     }
   }
 `;
