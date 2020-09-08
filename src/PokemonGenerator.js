2f14fd6068d51676f7e116964f4b52fa9389fe75
Rem Kim <kim.rmi.93@gmail.com>
2020-09-08 14:25:37 -0400

    solution
    Signed-off-by: Rem Kim <kim.rmi.93@gmail.com>

--- src/PokemonGenerator.js
+++ src/PokemonGenerator.js
@@ -1,19 +1,46 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import {
   Query, // component
-  useQuery // hook
 } from 'urql';
 import getPokemons from './api/getPokemons';
 
-const PokemonGenerator = () =>
-{
-  return (
-  <Fragment>
-    <h1>🤔</h1>
-    <p>
-      Hmm... Needs more Pokemon
-    </p>
-  </Fragment>
-)}
+class PokemonGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomPokemonIdx: Math.round(Math.random() * 150)
+    };
+    this.randomizePokemon = this.randomizePokemon.bind(this)
+  }
+
+  randomizePokemon(){
+    this.setState({
+      randomPokemonIdx: Math.round(Math.random() * 150)
+    })
+  }
+
+  render() {
+    return (
+      <Query query={getPokemons}>
+        {({ fetching, error, data }) => {
+          if (error) return 'Oops';
+          if (fetching) return 'Loading';
+          const { randomPokemonIdx } = this.state;
+          const pokemon = data.pokemons[randomPokemonIdx];
+          return (
+            <div>
+              <h1>{pokemon.name}</h1>
+              <h2>{pokemon.classification}</h2>
+              <p >
+                <img src={pokemon.image} alt={pokemon.name} style={{height: '250px'}} />
+              </p>
+              <button onClick={this.randomizePokemon}>Gimme Pokemon!</button>
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
+}
 
 export default PokemonGenerator;
